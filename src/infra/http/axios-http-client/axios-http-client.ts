52c0d8848e498a7cd44e6a7d81b10cbc14995780
BrunnoM7 <brunno.marchetti7@gmail.com>
2024-12-09 15:53:35 -0300

    feat: ensure Axios HttpClient calls axios with corret URL

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,9 @@
+import { HttpPostParams } from "@/data/protocols/http";
+import axios from 'axios'
+
+export class AxiosHttpClient {
+  async post (params: HttpPostParams<any>): Promise<void> {
+    // Work around in order to ensure it is typed correctly
+    await axios(params as unknown as Axios.AxiosXHRConfig<HttpPostParams<any>>)
+  }
+}
