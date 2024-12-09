beed2c7a78fa004192287573db361fa3619aca29
BrunnoM7 <brunno.marchetti7@gmail.com>
2024-12-09 16:00:11 -0300

    fix: pass correct parameter to axios on AxiosHttpClient

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -4,6 +4,6 @@ import axios from 'axios'
 export class AxiosHttpClient {
   async post (params: HttpPostParams<any>): Promise<void> {
     // Work around in order to ensure it is typed correctly
-    await axios(params as unknown as Axios.AxiosXHRConfig<HttpPostParams<any>>)
+    await axios(params.url as unknown as Axios.AxiosXHRConfig<HttpPostParams<any>>)
   }
 }
