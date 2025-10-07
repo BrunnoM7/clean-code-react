9a84fad7b73547f4b9cf8e5af709496121424e02
BrunnoM7 <brunno.marchetti7@gmail.com>
2025-10-06 23:33:34 -0300

    refactor: move logo to a separate file

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -3,10 +3,12 @@ import Styles from './spinner-styles.scss'
 
 type Props = React.HTMLAttributes<HTMLElement>
 
-export const Spinner: React.FC<Props> = (props: Props) => {
+const Spinner: React.FC<Props> = (props: Props) => {
   return (
     <div {...props} className={[Styles.spinner, props.className].join(' ')}>
       <div/><div/><div/><div/>
     </div>
   )
 }
+
+export default Spinner
