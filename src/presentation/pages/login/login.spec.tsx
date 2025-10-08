90d9b855e7898caf03b242c2c1e11bb1833bcdbf
BrunnoM7 <brunno.marchetti7@gmail.com>
2025-10-08 09:00:12 -0300

    test: ensure login start with input with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -6,19 +6,18 @@ describe('Login component', () => {
   test('Should start with initial state', () => {
     const { getByTestId } = render(<Login />);
     const errorWrap = getByTestId('error-wrap');
-
     expect(errorWrap.childElementCount).toBe(0);
 
     const submitButton = getByTestId('submit') as HTMLButtonElement;
-
     expect(submitButton.disabled).toBe(true);
 
-    // const emailInput = screen.getByLabelText(/email/i);
-    // const passwordInput = screen.getByLabelText(/password/i);
-    // const submitButton = screen.getByRole('button', { name: /submit/i });
+    const emailStatus = getByTestId('email-status');
+    expect(emailStatus.title).toBe('Campo obrigatório');
+    expect(emailStatus.textContent).toBe('🔴');
+    
+    const passwordStatus = getByTestId('password-status');
+    expect(passwordStatus.title).toBe('Campo obrigatório');
+    expect(passwordStatus.textContent).toBe('🔴');
 
-    // expect(emailInput).toBeInTheDocument();
-    // expect(passwordInput).toBeInTheDocument();
-    // expect(submitButton).toBeInTheDocument();
   });
 })
