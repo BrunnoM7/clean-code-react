a1729d0c0d5b5f929045a1d7f748d7d8b56f92a6
BrunnoM7 <brunno.marchetti7@gmail.com>
2025-10-13 22:42:47 -0300

    test: ensure ValidationComposite returns first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -6,14 +6,14 @@ describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
     const validationSpy = new ValidationSpy('any_field');
     const validationSpy2 = new ValidationSpy('any_field');
-
-    validationSpy2.error = new Error('any_error_message');
+    validationSpy.error = new Error('first_error_message');
+    validationSpy2.error = new Error('second_error_message');
 
     const sut = new ValidationComposite([
       validationSpy,
       validationSpy2
     ]);
     const error = sut.validate('any_field', 'any_value');
-    expect(error).toBe('any_error_message');
+    expect(error).toBe('first_error_message');
   });
 });
