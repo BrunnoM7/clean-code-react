4ee68a2fd8711b62c5259f343d33ca6309c34a5e
BrunnoM7 <brunno.marchetti7@gmail.com>
2025-10-14 00:11:08 -0300

    feat: ensure ValidationBuilder has a minLength helper

--- src/validation/validators/index.ts
+++ src/validation/validators/index.ts
@@ -2,3 +2,4 @@ export * from "@/validation/validators/builder/validation-builder";
 export * from "@/validation/validators/validation-composite/validation-composite";
 export * from "@/validation/validators/email/email-validation";
 export * from "@/validation/validators/required-field/required-field-validation";
+export * from "@/validation/validators/min-length/min-length-validation";
